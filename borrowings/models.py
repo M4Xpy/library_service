92ac6d1fa6b2a9dfe5a35e40fbd8c64264227c42
Maxim V <m4k51m15k4m@gmail.com>
2024-02-26 13:26:08 +0200

    add Borrowing to borrowings/models.py and admin.py

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True, )
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True, )
+    book_id = models.IntegerField()
+    user_id = models.IntegerField()
+
+    def __str__(self):
+        return f"Borrowing ID: {self.id}"
