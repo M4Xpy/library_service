c3d04736f96bc990c15ab7eccad237e8f9341cd4
Maxim V <m4k51m15k4m@gmail.com>
2024-02-26 13:07:42 +0200

    complete books/urls.py and views.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from books.models import Book
+from books.permissions import IsAdminOrReadOnly
+from books.serializers import BookSerializer
+
+
+class BookViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = IsAdminOrReadOnly,
