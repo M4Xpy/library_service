07011735cdc9341a82b8ccdbd99b60ffb8477fd7
Maxim V <m4k51m15k4m@gmail.com>
2024-02-26 12:00:59 +0200

    add Book(models.Model) books/models.py

--- books/models.py
+++ books/models.py
@@ -1,3 +1,23 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "HARD"
+        SOFT = "SOFT"
+
+    title = models.CharField(max_length=255, )
+    author = models.CharField(max_length=255, )
+    cover = models.CharField(max_length=4, choices=CoverChoices.choices, )
+    inventory = models.PositiveIntegerField(default=0, )
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2, )
+
+    def clean(self):
+        if self.inventory < 0:
+            raise ValidationError("Inventory must be a non-negative integer.")
+        if self.daily_fee < 0:
+            raise ValidationError("Daily fee must be a non-negative decimal value.")
+
+    def __str__(self):
+        return self.title
