b0f1f5c9adb61520ea379164bf22b2d3f555ce59
Maxim V <m4k51m15k4m@gmail.com>
2024-02-27 17:25:11 +0200

    add views_tests.py for  Payment

--- payments/views.py
+++ payments/views.py
@@ -2,9 +2,10 @@ from rest_framework import viewsets
 
 from payments.models import Payment
 from payments.serializers import PaymentSerializer
+from users.permissions import IsAdminOrIfAuthenticatedReadOnly
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    # permission_classes = IsAdminOrIfAuthenticatedReadOnly,
+    permission_classes = IsAdminOrIfAuthenticatedReadOnly,
