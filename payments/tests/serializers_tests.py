4d6a83459c53d20981e838da2a51727fa4895453
Maxim V <m4k51m15k4m@gmail.com>
2024-02-27 17:17:20 +0200

    add serializers_tests.py for  Payment

---
+++ payments/tests/serializers_tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
+
+
+class PaymentSerializerTestCase(TestCase):
+    def test_serializer_data(self):
+        payment = Payment.objects.create(
+            status=Payment.StatusChoices.PENDING,
+            payment_type=Payment.TypeChoices.PAYMENT,
+            borrowing_id=1,
+            session_url="http://example.com",
+            session_id=123,
+            money_to_pay=10.0
+        )
+        serializer = PaymentSerializer(payment)
+
+        expected_data = {
+            "id": payment.id,
+            "status": Payment.StatusChoices.PENDING,
+            "payment_type": Payment.TypeChoices.PAYMENT,
+            "borrowing_id": 1,
+            "session_url": "http://example.com",
+            "session_id": 123,
+            "money_to_pay": "10.00"
+        }
+        self.assertEqual(serializer.data, expected_data)
